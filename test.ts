b5c1ab10e15a809b1884ed1cb0cde8fe12f6da2f
Neko Of The Abyss <psi@innocent.com>
2022-06-15 21:55:17 +0530

    test .at()

---
+++ test.ts
@@ -0,0 +1,18 @@
+import Pokemon from "https://deno.land/x/fortuna@v1.1.2/testdata/pokemon.json" assert {
+  type: "json",
+};
+
+interface PokemonData {
+    name: string,
+    id: number,
+    tier: string,
+}
+
+import { BetterMap } from "./mod.ts"
+
+const b = new BetterMap<string, PokemonData>("Pokemon")
+for(const pokemon of Pokemon) {
+    b.set(pokemon.name, pokemon)
+}
+
+console.log(b.at(-3237))
