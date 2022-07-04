a0e509deedf48018d5721023b4fc808729f1fc3c
Neko Of The Abyss <psi@innocent.com>
2022-07-04 18:14:43 +0530

    add multi-item fetching

--- mod.ts
+++ mod.ts
@@ -12,29 +12,31 @@ export class BetterMap<K, V> extends Map<K, V> {
     this.name = typeof name === "string" ? name : "unknown items";
   }
   /**
-   * Convert the map into an array of values
+   * Convert the map into an array of values / keys
    * @returns Array of items in the map
    */
-  array() {
-    return this.map<V>((x: V) => x);
+  array(): V[];
+  array(keys: boolean): K[];
+  array(keys = false): K[] | V[]  {
+    return keys ? this.map<K>((_: V, x: K) => x) : this.map<V>((x: V) => x);
   }
   /**
-   * Return the nth element of the map. 
+   * Return the nth element of the map.
    * @param {number} pos - Position to get data.
    * @returns {V} Item at specified index.
    */
   at(pos: number): V | undefined {
-    if(pos > (this.size - 1)) pos = pos % (this.size - 1);
-    if(pos < 0) pos = (this.size) + (pos % (this.size - 1))
-    const val = this.values()
-    for(let i = 0; i < pos; ++i) {
-      val.next()
+    if (pos > (this.size - 1)) pos = pos % (this.size - 1);
+    if (pos < 0) pos = (this.size) + (pos % (this.size - 1));
+    const val = this.values();
+    for (let i = 0; i < pos; ++i) {
+      val.next();
     }
-    return val.next().value
+    return val.next().value;
   }
   /**
    * Array#every but for a Map
-   * @param fn - Function to run on every element.
+   * @param fn Function to run on every element.
    * @returns {boolean} True or false
    */
   every(fn: (v: V, k: K) => boolean): boolean {
@@ -47,7 +49,7 @@ export class BetterMap<K, V> extends Map<K, V> {
   }
   /**
    * Array#filter but for a Map
-   * @param fn - Function to be passed.
+   * @param fn Function to be passed.
    * @returns {BetterMap} BetterMap with elements that passed.
    */
   filter(fn: (v: V, k: K) => boolean): BetterMap<K, V> {
@@ -60,7 +62,7 @@ export class BetterMap<K, V> extends Map<K, V> {
     return newColl;
   }
   /**
-   * @param fn - Function to be passed.
+   * @param fn Function to be passed.
    * @returns A value from the map. If none found, returns undefined.
    */
   find(fn: (v: V, k: K) => boolean): V | undefined {
@@ -72,18 +74,32 @@ export class BetterMap<K, V> extends Map<K, V> {
     return undefined;
   }
   /**
-   * Get the first element from the map.
+   * Get the first element(s) from the map.
+   * @param {number} n Number of elements to fetch.
    * @returns The first element / undefined.
    */
-  first(): V | undefined {
-    return this.values().next().value;
+  first(): V | undefined;
+  first(n: number): V[];
+  first(n?: number): V | V[] | undefined {
+    const iter = this.values();
+    if (n && n > this.size) return;
+    return (typeof n === "number" && !isNaN(n))
+      ? Array.from({ length: n }, () => iter.next().value)
+      : iter.next().value;
   }
   /**
-   * Get the first element's key from the map.
+   * Get the first (n) element's key from the map.
+   * @param {number} n Number of elements to fetch.
    * @returns The first element's key / undefined.
    */
-  firstKey(): K | undefined {
-    return this.keys().next().value;
+  firstKey(): V | undefined;
+  firstKey(n: number): V[];
+  firstKey(n?: number): V | V[] | undefined {
+    const iter = this.keys();
+    if (n && n > this.size) return;
+    return (typeof n === "number" && !isNaN(n))
+      ? Array.from({ length: n }, () => iter.next().value)
+      : iter.next().value;
   }
   /**
    * Convert the key-value pairs into key-value pairs... I mean a JavaScript object.
@@ -92,16 +108,35 @@ export class BetterMap<K, V> extends Map<K, V> {
   json(): Record<string, V> {
     const json: Record<string, V> = {};
     for (const item of this.entries()) {
-      json[`${item[0]}`] = item[1]
-//      Object.defineProperty(json, `${item[0]}`, {
-//        value: item[1],
-//     });
+      json[`${item[0]}`] = item[1];
+      //      Object.defineProperty(json, `${item[0]}`, {
+      //        value: item[1],
+      //     });
     }
     return json;
   }
   /**
+   * Get last value(s) in the Map.
+   */
+  last(): V | undefined;
+  last(n: number): V[];
+  last(n?: number): V | V[] | undefined {
+    const arr = this.array();
+    return (typeof n === "number" && !isNaN(n)) ? arr.slice(-n) : arr[arr.length - 1];
+  }
+  /**
+   * Get last key(s) in the Map.
+   */
+  lastKey(): K | undefined;
+  lastKey(n: number): K[];
+  lastKey(n?: number): K | K[] | undefined {
+    const arr = this.array(true);
+    return (typeof n === "number" && !isNaN(n)) ? arr.slice(-n) : arr[arr.length - 1];
+  }
+
+  /**
    * Map the Map into an Array.
-   * @param fn - Function for mapping.
+   * @param fn Function for mapping.
    * @returns {T[]} Array.
    */
   map<T>(fn: (v: V, k: K) => T): T[] | [] {
@@ -113,7 +148,7 @@ export class BetterMap<K, V> extends Map<K, V> {
   }
   /**
    * Get a random element from the BetterMap.
-   * @param fn - Function to run on every element.
+   * @param fn Function to run on every element.
    * @returns {boolean} True or false
    */
   random(): V | undefined;
@@ -136,8 +171,8 @@ export class BetterMap<K, V> extends Map<K, V> {
     return iter.next().value;
   }
   /**
-   * Array#reduce but for a Map
-   * @param fn - Reducer function.
+   * Reduce data in the map.
+   * @param fn Reducer function.
    * @returns {T} Reduced data.
    */
   reduce<T>(fn: (acc: T, val: [K, V]) => T, first: T): T {
@@ -150,8 +185,8 @@ export class BetterMap<K, V> extends Map<K, V> {
     return result;
   }
   /**
-   * Array#some but for a Map
-   * @param fn - Function to run on every element.
+   * Check if at least one entry from the Map passes the function.
+   * @param fn Function to run on every element.
    * @returns {boolean} True or false
    */
   some(fn: (val: V, key: K) => boolean): boolean {
@@ -164,7 +199,7 @@ export class BetterMap<K, V> extends Map<K, V> {
   }
   /**
    * Sort elements in the better map.
-   * @param fn - Function to use for sorting.
+   * @param fn Function to use for sorting.
    * @returns {BetterMap<K, V>} sorted BetterMap.
    */
   sort(fn: (v1: V, v2: V, k1: K, k2: K) => number = () => 0): BetterMap<K, V> {
@@ -188,7 +223,7 @@ export class BetterMap<K, V> extends Map<K, V> {
   }
   /**
    * Create a new map from an existing Map or an array of key-value pairs
-   * @param data - Existing Map / Array of Key-Value pairs.
+   * @param data Existing Map / Array of Key-Value pairs.
    * @returns {BetterMap<K1, V1>}
    */
   static from<K1, V1>(data: Map<K1, V1> | [K1, V1][]): BetterMap<K1, V1> {
